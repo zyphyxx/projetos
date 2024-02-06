cd06093401c79d67925b2fae12f5fc6c7eb4fc38
Edgar Jesus <mendesbr92@gmail.com>
2024-02-05 22:18:24 -0300

    feat: melhorando a parte de retorno

--- view/dados.js
+++ view/dados.js
@@ -1,18 +1,30 @@
-const texto = document.getElementById('texto');
+const nome = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const btn = document.querySelector('button');
 
 function retornoUser() {
-  fetch("http://localhost:8080/cadastro",{
+  fetch("http://localhost:8080/cadastro", {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    method:"GET",
+    method: "GET",
   }).then(response => response.json()).then(users => {
-    users.forEach(users => {
-      texto.innerHTML += `-----DADOS------`
-      texto.innerHTML += `<p>Nome: ${users.name}</p>`
-      texto.innerHTML += `<p>Email: ${users.email}</p>`
-      texto.innerHTML += `<p>Phone: ${users.phone}</p>`
+    nome.innerHTML = ''; // Limpar o conteúdo anterior
+    email.innerHTML = '';
+    phone.innerHTML = '';
+
+    users.forEach(user => {
+      nome.innerHTML += `<div class="user-data"><p>${user.name}</p></div>`;
+      email.innerHTML += `<div class="user-data"><p>${user.email}</p></div>`;
+      phone.innerHTML += `<div class="user-data"><p>${user.phone}</p></div>`;
     });
-  }).catch(error => console.error('erro ao buscar usuarios:', error))
+
+  }).catch(error => console.error('Erro ao buscar usuários:', error));
 };
+
+btn.addEventListener('click', () => {
+  retornoUser(); // Chama a função para buscar e exibir dados
+  
+});
